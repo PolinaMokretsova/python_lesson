68b956f036472d2fe9bb5b2327baf7ed2a4222ec
PolinaMokretsova <pmokretsova@mail.ru>
2022-06-23 21:25:14 +0500

    edit function_name

--- python_homework.py
+++ python_homework.py
@@ -1,30 +1,25 @@
-# Сделайте функцию, которая будет печатать
-# читаемое имя переданной ей функции и ее аргументов.
-# Вызовите ее для трех функций ниже в цикле
-# Подсказка: Имя функции можно получить с помощью func.__name__
-
-def open_browser(arg1="Открываем браузер"):
+def open_browser(browser_name):
     pass
 
 
-
-def go_to_companyname_homepage(arg2="Домашняя страница"):
+def go_to_companyname_homepage(page_url):
     pass
 
 
-
-
-
-def find_registration_button_on_login_page(arg3="Находим кнопку регистрации на странице авторизации"):
+def find_registration_button_on_login_page(page_url, button_text):
     pass
 
 
-import inspect
+functions = [open_browser, go_to_companyname_homepage, find_registration_button_on_login_page]
 
-def function_name(*args):
-    for function in args:
-       arguments = inspect.signature(function)
-       print( "Имя функции:", function.__name__, "Ее понятное имя-аргумент", arguments)
 
-function_name(open_browser, go_to_companyname_homepage, find_registration_button_on_login_page)
+def function_name(func):
+    for arg in func:
+        print("Имя функции:", arg.__name__, end='. ')
+        if not any(arg.__code__.co_varnames):
+            print("No arguments")
+        else:
+            print("Аргумент функции:", arg.__code__.co_varnames)
 
+
+function_name(functions)
