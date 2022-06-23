a51d0a89b94aa8d54980837006cccb32d4b23ca9
PolinaMokretsova <pmokretsova@mail.ru>
2022-06-23 13:32:33 +0500

    edit function_name

--- python_homework.py
+++ python_homework.py
@@ -3,18 +3,28 @@
 # Вызовите ее для трех функций ниже в цикле
 # Подсказка: Имя функции можно получить с помощью func.__name__
 
-def open_browser():
+def open_browser(arg1="Открываем браузер"):
     pass
 
 
-def go_to_companyname_homepage():
+
+def go_to_companyname_homepage(arg2="Домашняя страница"):
     pass
 
 
-def find_registration_button_on_login_page():
+
+
+
+def find_registration_button_on_login_page(arg3="Находим кнопку регистрации на странице авторизации"):
     pass
 
-functions = (open_browser, go_to_companyname_homepage, find_registration_button_on_login_page)
-for g in functions:
-    string_name = g.__name__
-    print(string_name)
+
+import inspect
+
+def function_name(*args):
+    for function in args:
+       arguments = inspect.signature(function)
+       print( "Имя функции:", function.__name__, "Ее понятное имя-аргумент", arguments)
+
+function_name(open_browser, go_to_companyname_homepage, find_registration_button_on_login_page)
+
